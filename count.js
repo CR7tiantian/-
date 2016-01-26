4b6e83176768ca20b32cca2272fb6d8025ad0746
CR7tiantian <1508108789@qq.com>
2016-01-26 22:15:38 +0800

    Update count.js

--- count.js
+++ count.js
@@ -1,33 +1,45 @@
+var time = document.getElementById("time"); // 计时器文本框
+start = document.getElementById("start"); // 开始按扭
+pause = document.getElementById("pause"); // 暂停按钮
+reset = document.getElementById("reset"); // 重置按钮
+ss = 1;
+h = 0;
+m = 0;
+s = 0;
+interval = undefined;
 
-<!DOCTYPE html>
-<html>
-<head>
-    <title></title>
-    <meta charset="UTF-8">
-</head>
-<style>
-    #time {
-        color: red;
-        border: solid 2px blue;
-        height: 30px;
-        width: 150px;
-        font-size: 14px;
-    };
-    input.buttons {
-        width: 620px;
-        height:25px;
-    }
+function count() {
+	ss++;
+	if ((ss % 100) == 0) {
+		s++;
+		ss = 1;
+	};
+	if (s > 0 && (s % 60) == 0) {
+		m++;
+		s = 0;
+	};
+	if (m > 0 && (m % 60) == 0) {
+		h++;
+		m = 0;
+	};
 
-</style>
-<body>
-<div style="margin:20px 10px">
-    <input type="text" id="time" value="0:0:0:00">
-</div>
-<div style="margin:20px 10px">
-    <input id="start" type="button" class="buttons" value="开始">
-    <input id="pause" type="button" class="buttons" value="暂停">
-    <input id="reset" type="button" class="buttons" value="重置">
-</div>
-<script type="text/javascript" src="js文件/计时器.js"></script>
-</body>
-</html>
+	time.value = h + ":" + m + ":" + s + ":" + ss;
+};
+
+start.onclick = function() {
+	if (interval == undefined) {
+		interval = setInterval(count, 10); // setInterval的第一个参数是"count()"  或者是count，，不能是count()!!!
+	}
+};
+pause.onclick = function() {
+	clearInterval(interval);
+	interval = undefined;
+};
+reset.onclick = function() {
+	clearInterval(interval);
+	interval = undefined;
+	ss = 1;
+	h = m = s = 0;
+	time.value = "0:0:0:00";
+};
+
